7ec5723f4a6477cb141b813e5f60d4a1f3c50c6c
Jevans <jevansstanley@gmail.com>
2023-06-13 23:00:24 +0300

    bug: fix home controller errors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
+  def index
+    @products = Product.all
 
-    def index
-        @products = Product.all
-      
-        @products[0].image_url = 'image2.jpg'
-        @products[1].image_url = 'image3.jpg'
-        @products[2].image_url = 'image4.jpg'
-      end
-      
+    if @products.present?
+      @products[0].image_url = 'image2.jpg' if @products[0]
+      @products[1].image_url = 'image3.jpg' if @products[1]
+      @products[2].image_url = 'image4.jpg' if @products[2]
+    end
+  end
 end
